6fe7650e215d5dabeac722c3ee09af483ce3a7ed
Phil-Cleland <philemoncleland@gmail.com>
2022-05-09 16:05:30 +0000

    fixed the error 'The plugin google_maps_flutter requires a higher Android SDK version.'

--- lib/pages/CountryMap.dart
+++ lib/pages/CountryMap.dart
@@ -1,14 +1,27 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class CountryMap extends StatelessWidget {
-  const CountryMap({ Key? key }) : super(key: key);
 
+	  Completer<GoogleMapController> _controller = Completer();
+
+static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Country Map'),
       ),
+      body: GoogleMap(initialCameraPosition: _kGooglePlex,
+			onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },),
       
     );
   }
